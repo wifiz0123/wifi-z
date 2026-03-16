6fe5d872877ddc8fbcf9ef9b8e7f820482c0d40e
wifiz0123 <wifiz0123@gmail.com>
2026-03-16 16:46:11 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,2 +1,36 @@
-self.addEventListener("install", e => self.skipWaiting())
-self.addEventListener("activate", e => self.clients.claim())
+self.addEventListener("install",event=>{
+self.skipWaiting()
+})
+
+self.addEventListener("activate",event=>{
+self.clients.claim()
+})
+
+self.addEventListener("message",event=>{
+
+if(event.data && event.data.type==="notify"){
+
+self.registration.showNotification("wifi-z",{
+
+body:event.data.message,
+icon:"img/icon192.png",
+badge:"img/icon72.png",
+vibrate:[200,100,200],
+tag:"wifi-z",
+renotify:true
+
+})
+
+}
+
+})
+
+self.addEventListener("notificationclick",event=>{
+
+event.notification.close()
+
+event.waitUntil(
+clients.openWindow("index.html")
+)
+
+})
